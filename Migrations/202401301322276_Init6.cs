c42d0f2f749bdbead77eaffe7e9f6f0257926a1c
Artur <al.ok1@wp.pl>
2024-01-30 14:38:33 +0100

    Baza

---
+++ Migrations/202401301322276_Init6.cs
@@ -0,0 +1,29 @@
+﻿namespace Sklep.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init6 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ConfigDatas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Value = c.String(),
+                        Description = c.String(),
+                        isDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ConfigDatas");
+        }
+    }
+}
